484d07e265bb46f7b5f2bdbbd5978420fd216ee1
Kirsten <kegillam@ucdavis.edu>
2018-10-09 23:23:09 -0700

    button experiments

--- SplashScreenDemo/app/src/main/java/com/example/andra/splashscreendemo/DisplayMessageActivity.java
+++ SplashScreenDemo/app/src/main/java/com/example/andra/splashscreendemo/DisplayMessageActivity.java
@@ -1,18 +1,13 @@
 package com.example.andra.splashscreendemo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        Intent intent = new Intent(this, TestButtonActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_display_message);
     }
 }
