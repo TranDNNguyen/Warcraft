dc654da3eab55c433f92836cd7040fe09645480f
Chengzhi <43768098+fucz@users.noreply.github.com>
2018-11-13 17:30:11 -0800

    move all necessary files ready to work on merging -Fu

---
+++ Warcraft/app/src/main/java/com/android/ecs160/warcraft/UiFragment/FragmentFive.java
@@ -0,0 +1,44 @@
+package com.example.andra.splashscreendemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class FragmentFive extends Fragment {
+
+    private View v;
+    public FragmentFive(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View v =inflater.inflate(R.layout.fragment_five, container, false);
+
+        IconImage Icon = new IconImage(this.getContext());
+        ImageButton imgBtn = (ImageButton) v.findViewById(R.id.archer);
+        imgBtn.setImageBitmap(Icon.returnImage());
+
+        IconImage Icon6 = new IconImage(this.getContext());
+        ImageButton imgBtn6 = (ImageButton) v.findViewById(R.id.axethrower);
+        imgBtn6.setImageBitmap(Icon6.returnImage6());
+
+
+
+        //TextView tv = (TextView) v.findViewById(R.id.rightTextview);
+        //tv.setText("Can you read??");
+
+        return v;
+
+
+    }
+
+}
